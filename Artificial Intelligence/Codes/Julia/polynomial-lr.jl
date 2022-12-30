777218010e6dccf95f032ddf081fe694b59bc92b
a-mhamdi <a_mhamdi@outlook.com>
2022-12-30 23:58:44 +0100

    not yet polynomial

--- Artificial Intelligence/Codes/Julia/polynomial-lr.jl
+++ Artificial Intelligence/Codes/Julia/polynomial-lr.jl
@@ -1,5 +1,22 @@
+#= POLYNOMIAL LINEAR REGRESSION =#
+
+## Import Librairies
 using CSV, DataFrames
 using MLJ
 
+## Read Data From File
 df = CSV.read("../Datasets/Position_Salaries.csv", DataFrame)
-describe(df)
+schema(df)
+
+X = df.Level
+y = df.Salary
+
+## Partition Of Data
+train, test = partition(eachindex(y), 0.8, shuffle=true, rng=123)
+Xtrain, Xtest = X[:, train], X[:, test]
+ytrain, ytest = y[train], y[test]
+
+## Load Linear Regressor
+LR = @load LinearRegressor pkg=MLJLinearModels
+lr = LR()
+
